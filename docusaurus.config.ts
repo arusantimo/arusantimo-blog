e735c3d43d35a316f5100bc0683c2ed00cf53eae
ziziana0507 <ziziana0507@ncsoft.com>
2024-04-03 15:58:13 +0900

    초기 설정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,23 +7,18 @@ const config: Config = {
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://arusantimo.github.io/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+
   baseUrl: '/arusantimo-blog/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
+
   organizationName: 'arusantimo', // Usually your GitHub org/user name.
   projectName: 'arusantimo-blog', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
+
   i18n: {
     defaultLocale: 'ko',
     locales: ['ko'],
